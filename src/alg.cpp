f9f1883f86a82b19c58011fb24b15988f6a658f4
deniskr12 <kremnden@gmail.com>
2024-03-28 20:37:12 +0300

    ADS-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,31 @@
 // Copyright 2021 NNTU-CS
 
+#include <cstdint>
+
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    uint64_t left = 0;
+    uint64_t right = size - 1;
+    uint64_t count = 0;
+    while (left <= right) {
+        uint64_t mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            count++;
+            uint64_t i = mid - 1;
+            while (i >= 0 && arr[i] == value) {
+                count++;
+                i--;
+            }
+            i = mid + 1;
+            while (i < size && arr[i] == value) {
+                count++;
+                i++;
+            }
+            return count;
+        } else if (arr[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return count;
 }
